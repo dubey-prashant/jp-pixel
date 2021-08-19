ff969b1c3836aa62a4a3ee04af4076c9e45e6a7f
Prashant Dubey <dubeyprashant583@gmail.com>
2021-08-20 01:02:56 +0530

    update content

--- data/services.js
+++ data/services.js
@@ -2,32 +2,42 @@
 module.exports = services = [
   {
     iconClass: ' fas fa-laptop-code fa-3x',
-    name: 'Web Design',
-    info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustrys'
+    name: 'Web Design & Development',
+    info: 'Allow us to make you a beautiful website with the help of our expertise. Make beautiful first impressions with our services. Build the highest converting websites and have a highly professional presence on the internet. Enable us to deliver you the best website with our creative assistance now!'
   },
   {
     iconClass: 'fas fa-mobile-alt fa-3x mb-4',
     name: 'App Development',
-    info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustrys'
+    info: 'Leverage the power of our blazing-fast coding skills and our passionate team who is about to deliver the best applications ever. Get the most dependable applications made with a terrific User Interface and User Experience to provide your audience with the best app experience ever. Enable us to create a brilliant app for you now!'
   },
   {
     iconClass: 'fas fa-magic fa-3x mb-4',
     name: 'Graphic Design',
-    info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustrys'
+    info: 'Websites & Apps are definitely imperative for your brand, but the designing aspect is more important. Visually appealing content can generate tons of leads. It can also help in retaining your audience who may become your future customers. What are you waiting for? Get stunning graphic designs that are visually appealing for your business now!'
   },
   {
     iconClass: ' fas fa-camera-retro fa-3x mb-4',
     name: 'Photo editing',
-    info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustrys'
+    info: 'Do you have pictures but you realize that they do not look the best? Do not worry; JP Pixels has got your back. With our photo editing services, transform your pictures into their best versions! Whether it is for your business or personal use, we always end up editing most impressively! Get your pictures transformed with us now!'
   },
   {
     iconClass: ' fab fa-youtube fa-3x mb-4',
     name: 'Video Editing',
-    info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustrys'
+    info: 'Did you know? Video content is thriving on the internet, and you do not want to miss that opportunity because of an unedited video, do you? Get the best-edited videos tailored to your requirements and scale up your content game with us!'
   },
   {
     iconClass: ' fas fa-pencil-alt fa-3x mb-4',
     name: 'Content Writing',
-    info: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been theindustrys'
+    info: 'Content is the King that rules the Internet. In today\'s times having binge- worthy content that engages has become a necessity to attract your target audience.We have the most well - researched as well as error - free content that educates and impresses.Get the best quality content writing services now that shall definitely help you rule the Content Kingdom.'
   },
+  {
+    iconClass: ' fab fa-uncharted fa-3x mb-4',
+    name: 'Software Development',
+    info: 'Meet your customer\'s needs by providing them convenient access on an encrypted platform. We provide you with an easily trackable CRM System to help your business flourish with a tremendous amount of sales. Skyrocket your staff\'s performance too with auto generated reports of all the departments of your company such as IT, Marketing, Finance, Operations, Human Resource.'
+  },
+  {
+    iconClass: 'fab fa-searchengin fa-3x mb-4',
+    name: 'On Page SEO',
+    info: "Most of the online searches happen through search engines especially Google. Leverage this fact and make the most of your content by optimizing your pages. Get your business content optimized and eventually rank on Google. Improve your reach by getting quality traffic with our On Page SEO Services now!"
+  }
 ]
